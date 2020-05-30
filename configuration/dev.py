98fcc807520aa04022b5e5f66ae6967f9d49019f
Subhadip Hazra <subhadip1997hazra@gmail.com>
2020-05-30 21:18:43 +0530

    master

--- configuration/dev.py
+++ configuration/dev.py
@@ -2,5 +2,5 @@ import os
 DEBUG=True
 SECRET_KEY='topsecret'
 SQLALCHEMY_DATABASE_URI='sqlite:///C:\\Users\\bigGs\\Desktop\\flask2\\databases\\BookCatalog.db'
-UPLOAD_FOLDER = UPLOAD_FOLDER = os.path.abspath('.\\test_books')
+UPLOAD_FOLDER = os.path.abspath('.\\test_books')
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
